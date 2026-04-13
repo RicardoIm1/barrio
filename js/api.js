4f5b9121af279d6eac2e876166bb814240090011
Rick <96212256+RicardoIm1@users.noreply.github.com>
2026-04-13 14:54:08 -0600

    ok

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // ==================== API CLIENT CON JSONP ====================
 const API = {
-  baseUrl: 'https://script.google.com/macros/s/AKfycbyt8aSZUOGUa5KOE6EzrdY_Hka5S1h9AVpSzlxinBs9q7KhXJr8ECRHiM8mW4ueSC4P/exec',
+  baseUrl: 'https://script.google.com/macros/s/AKfycbyLKR4P6NofOykQZhNihKLXIhhMSOnUkg1RqvWccVf31n-ELpQ7UQmH2fe1a7Arp7tg/exec',
 
   get apiKey() {
     return localStorage.getItem('api_key');
