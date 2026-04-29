b34745290471a8f416edc28c17519e770819894d
Rick <96212256+RicardoIm1@users.noreply.github.com>
2026-04-29 13:33:37 -0600

    OK

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -18,7 +18,7 @@ export async function handler(event) {
   try {
     const body = JSON.parse(event.body);
 
-    const resp = await fetch("https://script.google.com/macros/s/AKfycbykkuDoLMTPCctLAEQ1_14Z6mOmx3h8ig52_VaXSPNaUMaRaMatxq1_quceW05_9UEt/exec", {
+    const resp = await fetch("https://script.google.com/macros/s/AKfycbymAkb9J6BXWUftZxC8sJAisCWznqnvnpv4_B9jCOMa_Bj4dz_dhDVMHKsourD7WhyW/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body)
