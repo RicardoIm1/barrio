08618759c6f41143fc8d33a08ebf1269cf5748f1
Rick <96212256+RicardoIm1@users.noreply.github.com>
2026-05-20 11:24:24 -0600

    ok

--- js/api.js
+++ js/api.js
@@ -11,7 +11,7 @@
 
 // ==================== API CLIENT - Jardines PVR ====================
 
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxC5WzySiQ0aPJWn0JZx22NIMp8d3n6G22W5GUuGBx4YapG0S6aRqEaMe71aHbKihuo/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzYHyORKM8Xu55k6NMbnFmEniveGt1__WO28qVoU5n2F1MEqsqbYtT_UCzI7U2yw-8U/exec';
 
 // Al inicio del archivo, después de const API_BASE_URL
 if (window.location.hostname !== 'localhost') {
--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -18,7 +18,7 @@ export async function handler(event) {
   try {
     const body = JSON.parse(event.body);
 
-    const resp = await fetch("https://script.google.com/macros/s/AKfycbxC5WzySiQ0aPJWn0JZx22NIMp8d3n6G22W5GUuGBx4YapG0S6aRqEaMe71aHbKihuo/exec", {
+    const resp = await fetch("https://script.google.com/macros/s/AKfycbzYHyORKM8Xu55k6NMbnFmEniveGt1__WO28qVoU5n2F1MEqsqbYtT_UCzI7U2yw-8U/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body)
