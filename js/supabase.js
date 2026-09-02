57b51136495c11575d14c8dc46f99cce39cdfec5
Rick <96212256+RicardoIm1@users.noreply.github.com>
2026-09-02 11:11:28 -0600

    ok

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://gnjaumpjerbbwlkcgxqa.supabase.co';
+/* const SUPABASE_URL = 'https://gnjaumpjerbbwlkcgxqa.supabase.co'; */
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduamF1bXBqZXJiYndsa2NneHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MjQ5ODksImV4cCI6MjEwMzUwMDk4OX0.B6QRJN4gg1NuTmv-RyFBeWQaTmlFUoOYDZlkYaiFUjU';
 
 const supabase = window.supabase.createClient(
